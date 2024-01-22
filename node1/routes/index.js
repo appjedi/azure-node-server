a7955ed233ac8aae3c0851058b5c99a6da82ea1d
Bob T <54815862+appjedi@users.noreply.github.com>
2024-01-22 12:42:03 -0800

    update repo
    update repo

--- node1/routes/index.js
+++ node1/routes/index.js
@@ -21,4 +21,50 @@ router.post("/profile", (req, res) => {
   Profile.create(data);
   res.send({ status: 1, message: "Profile Created" });
 })
+router.get("/profile/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = await Profile.findById(id);
+  console.log(data);
+
+  res.send(data);
+});
+router.post("/login", async (req, res) => {
+  const data = { email: req.body.email, password: req.body.password }
+  console.log("login", data);
+  const users = await Profile.find({ email: data.email });
+  console.log(users);
+  if (users != null && users.length > 0 && users[0].password === data.password) {
+    const user = { id: users[0]._id, lastName: users[0].lastName, firstName: users[0].lastName, email: users[0].email }
+    ssn = req.session;
+    ssn.user = user;
+    res.send({ user: user, status: 1, message: "user authenticated" });
+  } else {
+    res.send({ status: -1, message: "user not authenticated" });
+  }
+})
+router.get("/user", (req, res) => {
+  ssn = req.session;
+  console.log(ssn.user);
+  res.send(ssn.user);
+})
+
+router.post("/post", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  Post.create(data);
+  res.send({ status: 1, message: "Profile Created" });
+});
+router.get("/posts", async (req, res) => {
+  const data = await Post.find({});
+  console.log(data);
+
+  res.send(data);
+});
+router.get("/post/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = await Post.findById(id);
+  console.log(data);
+
+  res.send(data);
+});
 module.exports = router;
