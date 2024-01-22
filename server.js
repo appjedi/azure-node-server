43d52878edcf414c73afa99208741ece4a275db8
Bob T <54815862+appjedi@users.noreply.github.com>
2024-01-21 16:17:36 -0800

    update repo
    update repo

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const port = process.env.port || 8080; // port update
+const port = process.env.port || 443; // port update
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const express = require("express");
 const session = require('express-session');
 const path = require("path");
-const Profile = require('./models/profile');
+const Profile = require('./models/Profile');
+const Post = require('./models/Post');
 const app = express();
 app.use(session({ secret: 'XASDASDA' }));
 
@@ -12,7 +13,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-const GC_RELEASE = "2024-01-15";
+const GC_RELEASE = "2024-01-18";
 //const MongoURL = "mongodb://localhost:27017/tom";
 const MongoURL = "mongodb+srv://appuser:AppData2022@cluster0.aga82.mongodb.net/tombook";
 mongoose.connect(MongoURL);
@@ -22,15 +23,61 @@ app.get("/release", (req, res) => {
   res.send(GC_RELEASE);
 });
 app.post("/profile", (req, res) => {
-    const data = req.body;
-    console.log(data);
-    Profile.create(data);
-    res.send({status:1, message:"Profile Created"});
+  const data = req.body;
+  console.log(data);
+  Profile.create(data);
+  res.send({ status: 1, message: "Profile Created" });
 })
-app.get("/profile", async (req, res) => {
+app.get("/profiles", async (req, res) => {
   const data = await Profile.find({});
   console.log(data);
-    
+
+  res.send(data);
+});
+app.get("/profile/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = await Profile.findById(id);
+  console.log(data);
+
+  res.send(data);
+});
+app.post("/login", async (req, res) => {
+  const data = { email: req.body.email, password: req.body.password }
+  console.log("login", data);
+  const users = await Profile.find({ email: data.email });
+  console.log(users);
+  if (users != null && users.length > 0 && users[0].password === data.password) {
+    const user = { id: users[0]._id, lastName: users[0].lastName, firstName: users[0].lastName, email: users[0].email }
+    ssn = req.session;
+    ssn.user = user;
+    res.send({ user: user, status: 1, message: "user authenticated" });
+  } else {
+    res.send({ status: -1, message: "user not authenticated" });
+  }
+})
+app.get("/user", (req, res) => {
+  ssn = req.session;
+  console.log(ssn.user);
+  res.send(ssn.user);
+})
+
+app.post("/post", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  Post.create(data);
+  res.send({ status: 1, message: "Profile Created" });
+});
+app.get("/posts", async (req, res) => {
+  const data = await Post.find({});
+  console.log(data);
+
+  res.send(data);
+});
+app.get("/post/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = await Post.findById(id);
+  console.log(data);
+
   res.send(data);
 });
 app.use(express.static("./src/build"));
